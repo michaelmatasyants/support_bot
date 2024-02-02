2902feb059057e6c27b0ad6c10573de360b6c1ee
Michael Matasyants <michael.matasyanc@gmail.com>
2024-02-02 08:09:21 +0300

    Add echobot for VK

--- vk_bot/bot.py
+++ vk_bot/bot.py
@@ -1,16 +1,22 @@
-import vk_api
+import random
+
+import vk_api as vk
 from django.conf import settings
 from vk_api.longpoll import VkEventType, VkLongPoll
 
 
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
+
+
 def main():
-    vk_session = vk_api.VkApi(token=settings.VK_GROUP_TOKEN)
+    vk_session = vk.VkApi(token=settings.VK_GROUP_TOKEN)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
